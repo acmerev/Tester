9eb0bfa9141c7eee2b83f3588a38bb82391fb889
Emmanuel Montiel Cruz <acmerev@gmail.com>
2022-07-15 23:41:38 -0500

    Create ejercicios.js

---
+++ ejercicios.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number} low
+ * @param {number} high
+ * @return {number}
+ */
+var countOdds = function(low, high) {
+    
+    contador = 0;
+    for(let i = low; i<=high; i++)
+        {
+            if(i % 2 == 0){
+                continue;
+            }else {
+                contador += 1; 
+            }
+            
+        }
+    return contador;
+};
+
