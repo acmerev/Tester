2ac22a81be9c0187f71f0d1c039b66c2bd65f38c
Emmanuel Montiel Cruz <acmerev@gmail.com>
2022-07-15 23:42:12 -0500

    Update ejercicios.js

--- ejercicios.js
+++ ejercicios.js
@@ -18,3 +18,26 @@ var countOdds = function(low, high) {
     return contador;
 };
 
+function largestOfFour(arr) {
+
+  arr.sort(function(a, b){return a - b});
+  console.log(arr)
+  arr.pop();
+  arr.shift();
+  
+  let aver= arr.length;
+  let sum = 0;
+  let average = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+      sum += arr[i];
+  }
+  average = sum / aver;
+  return average;
+}
+
+let arr = [25000,48000,57000,86000,33000,10000,42000,3000,54000,29000,79000,40000]
+
+console.log(largestOfFour(arr))
+
+//largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, //37, 39], [1000, 1001, 857, 1]]);
